48a3a11e2f614f12a663dbb1032acedca20a86c9
clement-danso <clement.quoda@gmail.com>
2021-12-19 18:24:28 +0000

    Changes to be committed: 	modified:   python27_install.sh

--- python27_install.sh
+++ python27_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ls#!/bin/bash
 
 #########################################
 # Installing python and necessary packages
@@ -9,29 +9,15 @@
 #########################################
 
 # installing python 2.7.3
-mkdir -p ~/local
-wget https://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
-tar xvzf Python-2.7.3.tgz
-cd Python-2.7.3
-./configure
-make
-make altinstall prefix=~/local  # specify local installation directory
-ln -s ~/local/bin/python2.7 ~/local/bin/python
-cd ..
+sudo apt update
+sudo apt install -y python2
 
-# install setuptools and pip for package management
-wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz#md5=7df2a529a074f613b509fb44feefe74e
-tar xvzf setuptools-0.6c11.tar.gz
-cd setuptools-0.6c11
-~/local/bin/python setup.py install  # specify the path to the python you installed above
-cd ..
-wget https://pypi.python.org/packages/source/p/pip/pip-1.2.1.tar.gz#md5=db8a6d8a4564d3dc7f337ebed67b1a85
-tar xvzf pip-1.2.1.tar.gz
-cd pip-1.2.1
-~/local/bin/python setup.py install  # specify the path to the python you installed above
+# installing pip
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
 
-# Now you can install other packages using pip
-~/local/bin/pip install requests  # install numpy
-~/local/bin/pip install urllib2
 
-~/local/bin/pip freeze  # to check python module version info
+# installing requests and urllib2
+pip install requests
+pip install urllib2
+
