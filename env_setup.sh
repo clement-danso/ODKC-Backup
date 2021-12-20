c76d2350b3c60cf3b11d948a26f17ec72a4019ab
clement-danso <clement.quoda@gmail.com>
2021-12-20 02:25:11 +0000

    Changes to be committed: 	modified:   env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -26,6 +26,7 @@ chmod +x ODKC_backup.sh
 
 
 # installing AWS CLI
+sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
