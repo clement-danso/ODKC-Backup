c54c402e972e01bb4266e1bcf2eb0ee90e5db8d2
clement-danso <clement.quoda@gmail.com>
2021-12-20 01:51:09 +0000

    Changes to be committed: 	modified:   ODKC_backup.sh

--- ODKC_backup.sh
+++ ODKC_backup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+
 cd ODKC-Backup
 
 python2 /home/quoda/ODKC-Backup/ODKC_backup.py
+
+
+
+sleep 5s
+
+# get name of last file
+lastbackupfile=`ls -Art | tail -n 1`
+
+# upload the latest backup to the server
+aws s3 cp “$lastbackupfile” s3://name_of_bucket/
+
+
